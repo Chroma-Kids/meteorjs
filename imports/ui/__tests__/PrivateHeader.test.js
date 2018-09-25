041f5b60df44ac90216e2bb6dacc32c485cbef4f
Angel Suarez Fernandez <titogelo@gmail.com>
2018-09-26 00:19:31 +0100

    added travis

--- imports/ui/__tests__/PrivateHeader.test.js
+++ imports/ui/__tests__/PrivateHeader.test.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { PrivateHeader } from '../PrivateHeader';
+import { Header } from '../common/Header';
 
 describe('PrivateHeader', function () {
 
   it('should set a button text to logout', function () {
-    const wrapper = shallow(<PrivateHeader title="Test title" handleLogout={() => { }} />);
+    const wrapper = shallow(<Header title="Test title" handleLogout={() => { }} />);
     const buttonText = wrapper.find('button').text();
 
     expect(buttonText).toBe('Logout');
@@ -13,7 +13,7 @@ describe('PrivateHeader', function () {
 
   it('should use title prop as an h1', function () {
     const title = 'Test title';
-    const wrapper = shallow(<PrivateHeader title={title} handleLogout={() => { }} />);
+    const wrapper = shallow(<Header title={title} handleLogout={() => { }} />);
     const actualTitle = wrapper.find('h1').text();
 
     expect(actualTitle).toBe(title);
@@ -21,7 +21,7 @@ describe('PrivateHeader', function () {
 
   it('should call handleLogout on click', function () {
     const spy = jest.fn();
-    const wrapper = shallow(<PrivateHeader title="Title" handleLogout={spy} />);
+    const wrapper = shallow(<Header title="Title" handleLogout={spy} />);
 
     wrapper.find('button').simulate('click');
 
