17bcae13858f8c6b85715d1ff3bc481de7e42f43
Angel Suarez Fernandez <titogelo@gmail.com>
2019-01-13 20:30:40 +0000

    added teachers-not-assgined api

---
+++ imports/api/teachers-not-assigned.js
@@ -0,0 +1,99 @@
+import { Meteor } from 'meteor/meteor';
+import { Mongo } from 'meteor/mongo';
+import moment from 'moment';
+import SimpleSchema from 'simpl-schema';
+
+export const TeachersNotAssigned = new Mongo.Collection('teachers-not-assigned');
+
+export const Methods = {
+  insert({ firstName, lastName }) {
+    if (!this.userId) {
+      throw new Meteor.Error('not-authorized');
+    }
+
+    const createdBy = Meteor.user();
+
+    new SimpleSchema({
+      firstName: {
+        type: String,
+        optional: false,
+        min: 1,
+      },
+      lastName: {
+        type: String,
+        optional: false,
+        min: 1,
+      },
+    }).validate({ firstName, lastName });
+
+    return TeachersNotAssigned.insert({
+      firstName,
+      lastName,
+      classroom_id: undefined,
+      createdBy: { _id: createdBy._id, username: createdBy.username },
+      createdAt: moment().valueOf(),
+      updatedAt: moment().valueOf(),
+    });
+  },
+  remove(_id) {
+    if (!this.userId) {
+      throw new Meteor.Error('not-authorized');
+    }
+
+    new SimpleSchema({
+      _id: {
+        type: String,
+        min: 1,
+      }
+    }).validate({ _id });
+
+    TeachersNotAssigned.remove({ _id });
+  },
+  update(_id, updates) {
+    if (!this.userId) {
+      throw new Meteor.Error('not-authorized');
+    }
+    const firstName = updates.firstName;
+    const lastName = updates.lastName;
+    const classroom_id = updates.classroom_id;
+
+    // TODO: Extract this and have just one validator?
+    new SimpleSchema({
+      _id: {
+        type: String,
+        min: 1,
+      },
+      firstName: {
+        type: String,
+        optional: true,
+      },
+      lastName: {
+        type: String,
+        optional: true,
+      },
+      classroom_id: {
+        type: String,
+        min: 1,
+      },
+    }).validate({ _id, firstName, lastName, classroom_id });
+
+    TeachersNotAssigned.update({ _id }, {
+      $set: { updatedAt: moment().valueOf(), firstName, lastName, classroom_id }
+    })
+  }
+}
+
+if (Meteor.isServer) {
+  Meteor.publish('teachers-not-assigned.all', function () {
+    if (!this.userId) {
+      return [];
+    }
+    return Teachers.find();
+  });
+}
+
+Meteor.methods({
+  'teachers-not-assigned.insert': Methods.insert,
+  'teachers-not-assigned.remove': Methods.remove,
+  'teachers-not-assigned.update': Methods.update
+});
